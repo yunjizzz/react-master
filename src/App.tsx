15ca27376cff21af13d5484c85cafb47e8b7ac34
yunji <yunjigo92@gmail.com>
2022-12-29 13:10:03 +0900

    Add basic react
    - 리액트 기본 문법 습득하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Circle from "./Circle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return (
+        <div>
+            <Circle borderColor="yellow" bgColor= "teal"/>
+            <Circle text="얘는 넘겨줌" bgColor= "tomato"/>
+        </div>
+    );
 }
 
 export default App;
