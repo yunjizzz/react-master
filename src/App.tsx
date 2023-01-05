e763e72c27a2a1caf23fafc894958a71faea2659
yunji <yunjigo92@gmail.com>
2023-01-05 17:52:40 +0900

    #5.7~5.8 Nested Routes
    - 디자인 추가 및 price, chart tab 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 
-
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-font-family: 'Source Sans Pro', sans-serif;
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -52,29 +50,28 @@ table {
   border-collapse: collapse;
   border-spacing: 0;
 }
-
-*{
-    box-sizing: border-box;
+* {
+  box-sizing: border-box;
 }
-
-body{
-    font-family: 'Source Sans Pro', sans-serif;
-    background-color: ${props => props.theme.bgColor};
-    color: ${props => props.theme.textColor};
+body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor};
+  line-height: 1.2;
 }
 a {
-    text-decoration: none;
-    color: inherit;
+  text-decoration:none;
+  color:inherit;
 }
-`
+`;
 
 function App() {
   return (
     <>
-        <GlobalStyle/>
-        <Router/>
+      <GlobalStyle />
+      <Router />
     </>
-
   );
 }
 
