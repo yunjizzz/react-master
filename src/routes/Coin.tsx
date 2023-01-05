8d59b8bbc4ced0461c39f53e5bb9d448900e0eab
yunji <yunjigo92@gmail.com>
2023-01-05 17:07:05 +0900

    #5.7 Data Types
    - API에서 불러온 데이터 타입 정의하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -35,17 +35,78 @@ interface RouteState{
     name: string;
 }
 
+interface InfoData{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData{
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD:{
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        }
+    };
+}
+
 function Coin() {
     const { coinId } = useParams<RouteParams>();
-    const [loading, setLoading] = useState(true);
+    const [loading,setLoading] = useState(true);
     const {state} = useLocation<RouteState>(); // dom에서 보내주는 정보 확인 가능
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
     
     useEffect(()=>{
         (
            async () => {
-                const json = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-                const priceData =  await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json()
-                console.log(priceData);
+                const info = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+                const priceInfo =  await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json()
+                setInfo(info);
+                setPriceInfo(priceInfo);
+
+                console.log(priceInfo);
            }
         )();
     },[]);
